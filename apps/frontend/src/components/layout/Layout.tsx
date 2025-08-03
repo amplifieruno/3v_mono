91754174adaae7d8e319eaa68400aa9f7c510291
Maksim Kuznetsov <mak.kooz@gmail.com>
2025-08-03 13:37:58 +0200

    fixes styles

--- apps/frontend/src/components/layout/Layout.tsx
+++ apps/frontend/src/components/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex h-screen bg-background">
+    <div className="flex h-screen bg-gray-50">
       <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden">
         <Header />
